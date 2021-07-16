4dcb62bf017d10e3ab1e7caff8caf85e07d724c7
ThienHeo1012 <thienheok49@gmail.com>
2021-07-16 20:02:30 +0700

    commit lai

--- Thien-List/Checkout.js
+++ Thien-List/Checkout.js
@@ -16,25 +16,25 @@ window.onclick = function(e) {
   console.log(activeDropdown)
   if (e.target.tagName === 'LI' && activeDropdown){
     if (e.target.innerHTML === 'Master Card') {
-      document.getElementById('credit-card-image').src = 'https://dl.dropboxusercontent.com/s/2vbqk5lcpi7hjoc/MasterCard_Logo.svg.png';
+      document.getElementById('thien-credit-card-image').src = 'https://dl.dropboxusercontent.com/s/2vbqk5lcpi7hjoc/MasterCard_Logo.svg.png';
           activeDropdown.classList.remove('visible');
       activeDropdown = null;
-      e.target.innerHTML = document.getElementById('current-card').innerHTML;
-      document.getElementById('current-card').innerHTML = 'Master Card';
+      e.target.innerHTML = document.getElementById('thien-current-card').innerHTML;
+      document.getElementById('thien-current-card').innerHTML = 'Master Card';
     }
     else if (e.target.innerHTML === 'American Express') {
-         document.getElementById('credit-card-image').src = 'https://dl.dropboxusercontent.com/s/f5hyn6u05ktql8d/amex-icon-6902.png';
+         document.getElementById('thien-credit-card-image').src = 'https://dl.dropboxusercontent.com/s/f5hyn6u05ktql8d/amex-icon-6902.png';
           activeDropdown.classList.remove('visible');
       activeDropdown = null;
-      e.target.innerHTML = document.getElementById('current-card').innerHTML;
-      document.getElementById('current-card').innerHTML = 'American Express';      
+      e.target.innerHTML = document.getElementById('thien-current-card').innerHTML;
+      document.getElementById('thien-current-card').innerHTML = 'American Express';      
     }
     else if (e.target.innerHTML === 'Visa') {
-         document.getElementById('credit-card-image').src = 'https://dl.dropboxusercontent.com/s/ubamyu6mzov5c80/visa_logo%20%281%29.png';
+         document.getElementById('thien-credit-card-image').src = 'https://dl.dropboxusercontent.com/s/ubamyu6mzov5c80/visa_logo%20%281%29.png';
           activeDropdown.classList.remove('visible');
       activeDropdown = null;
-      e.target.innerHTML = document.getElementById('current-card').innerHTML;
-      document.getElementById('current-card').innerHTML = 'Visa';
+      e.target.innerHTML = document.getElementById('thien-current-card').innerHTML;
+      document.getElementById('thien-current-card').innerHTML = 'Visa';
     }
   }
   else if (e.target.className !== 'thien-dropdown-btn' && activeDropdown) {
